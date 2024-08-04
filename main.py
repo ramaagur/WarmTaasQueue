a3d2c1cb4de8e8c3238c3c2ed2d173710f7c14d5
ramaagur <39278830+ramaagur@users.noreply.github.com>
2024-08-04 05:34:48 +0000

    Movie proj-add

--- main.py
+++ main.py
@@ -393,7 +393,7 @@ else:
       print("Status is not BMW")       
 
 
-print("Find a number is aprime number")"""
+print("Find a number is aprime number")
 for a in range(1,15):
       for b in range(2,a):
             if a%b==0:
@@ -413,3 +413,52 @@ for a in range(1,12):
                   break
       else:
             print(f"this will be {a} is a prime number")
+            """
+"""                                                            
+Enter 'a' to add a movie, 'l' to see the movies, 'f' to find the movies and 'q' to quit the movies
+--Add Movies                                                   
+--show movies                                                  
+--find a movie                                                 
+-stop running the movie                                             
+Tasks:                                                                                                        
+[]: Decide where to store the movie                            
+[]: what is the format of the movie                            
+[]: show user the main interface and get their input           
+[]: Allow users to add movie                                   
+[]: show all their movies                                      
+[]: Find a  movie                                              
+[]: Stop running the program when they enter 'q'               
+"""          
+movies = []                 
+"""                         
+     movie = {              
+     'name':         (str), 
+     'director':     (str), 
+     'year':         (int)  
+     }                      
+"""
+def menu():            
+    user_input = input ( "Enter 'a', 'l' , 'f' and 'q'  :  " ) 
+    while user_input != 'q':
+        if user_input == 'a':                                                   
+              add_movie ()                                  
+        elif user_input == 'l':
+            show_movie ()           
+        elif user_input == 'f':
+              find_movie ()                                      
+        else:                                                  
+            print ( "unknown command" )                        
+        user_input = input ( "Enter 'a', 'l' , 'f' and 'q'  :  " )         
+
+def add_movie():                                                           
+    name = input ( "Enter a movie name  :  " )                             
+    director = input ( "Enter a directr name  :  " )                       
+    year = int ( input ( "Enter a movie name  :  " ) )                     
+
+    movies.append ({                                           
+        'name':name,                                           
+        'director':director,                                   
+        'year':year                                            
+    })                                                         
+menu ()                                                                    
+print(movies)
